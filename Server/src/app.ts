b64d749d726cc91e4f0f497440404b22c06c07e0
Roberto Loza <robertoloxa88@gmail.com>
2025-02-17 21:32:26 -0600

    update route api

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -19,7 +19,7 @@ Application.use(rateLimit(LimiterConfig))
 Application.use(BodyParser.json({ limit: "100mb" }))
 Application.use(BodyParser.urlencoded({ limit: "100mb", extended: true }))
 Application.use(fileUpload({ useTempFiles: true, tempFileDir: './Upload' }))
-Application.use('/backend/api/rest/ufostartserver/route/fetch/axios', Routes)
+Application.use('/backend/api/rest/ufostart/server/route/fetch/axios', Routes)
 Application.get('/favicon.ico', (req: Request, res: Response) => { res.status(204).end() });
 
 export default Application;
